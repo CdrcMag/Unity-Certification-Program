31aecb0b6967caccb0c1a18b7b24efa444419c48
CdrcMag <cdrcmag@outlook.fr>
2020-06-14 19:42:21 +0200

    Inital commit course 03

---
+++ Course03/Project/Stealth UCP C03 V04 – Enemy Navigation and Movement Challenge/Assets/__Scripts/Enemy_Movement.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;//For NavMesh
+
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy_Movement : MonoBehaviour
+{
+    //Enemy agent
+    private NavMeshAgent _agent;
+
+    //Destination
+    private Transform _goal;
+
+    //Waypoints
+    public Transform Waypoints;
+    private List<Transform> WaypointsList = new List<Transform>();
+
+    //List of waypoint to follow in that order
+    public List<int> WaypointsToFollow = new List<int>();
+    private int _nextWaypoint = 0;
+    bool sens = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+
+        //Every waypoints are stored in the list
+        setAllWaypoints();
+
+        _nextWaypoint = WaypointsToFollow[0];
+
+        goToWaypoints(_nextWaypoint);
+
+
+    }
+
+    
+
+    private void Update()
+    {
+        MovementLogic();
+    }
+
+
+
+
+    /// <summary>
+    /// Handles the enemy's movements
+    /// </summary>
+    void MovementLogic()
+    {
+
+        // Check if we've reached the destination
+        if (!_agent.hasPath)//If the enemy doesn't have a path
+        {
+
+            int time = 0;
+
+            time = WaypointsList[_nextWaypoint].GetComponent<Waypoints>().Waypoint_SecondsToWaitAt;
+
+            if (_nextWaypoint == WaypointsToFollow.Count - 1)
+            {
+                sens = false;
+            }
+            if (_nextWaypoint == 0)
+            {
+                sens = true;
+            }
+
+            Debug.Log("Reached waypoint " + _nextWaypoint);
+
+            if (sens)
+            {
+                _nextWaypoint++;
+            }
+            else
+            {
+                _nextWaypoint--;
+
+            }
+
+            
+
+            goToWaypoints(_nextWaypoint);
+
+            StartCoroutine(WaitOnPosition(time));
+            
+        }
+    }
+
+    IEnumerator WaitOnPosition(int time)
+    {
+        _agent.isStopped = true;
+        yield return new WaitForSeconds(time);
+        _agent.isStopped = false;
+
+    }
+
+
+
+    /// <summary>
+    /// Moves the enemy to the waypoint identified by its id
+    /// </summary>
+    void goToWaypoints(int id)
+    {
+        _agent.destination = WaypointsList[id].transform.position;
+    }
+
+
+
+    /// <summary>
+    /// Adds all waypoints' transforms to a list
+    /// </summary>
+    void setAllWaypoints()
+    {
+        foreach(Transform waypoints in Waypoints)
+        {
+            WaypointsList.Add(waypoints);
+            //Debug.Log(WaypointsList[WaypointsList.Count-1] + " added to Waypoints List");
+        }
+    }
+
+   
+}
---
+++ Course03/Project/Stealth UCP C03 V04 – Enemy Navigation and Movement Challenge/Assets/__Scripts/Waypoints.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    [Tooltip("To add a new waypoint, copy paste one where you want, give it the appropriate ID, and add this id to the list in the Enemy_Movement.cs component")]
+    [Header("Waypoint ID (0 to X) see tooltip")]
+    public int Waypoint_ID;
+    [Header("Time spent not moving when position reached")]
+    public int Waypoint_SecondsToWaitAt;
+
+    [HideInInspector]
+    public Vector3 Waypoint_Position;
+    [HideInInspector]
+    public Vector3 Waypoint_Rotation;
+
+
+
+    private void Awake()
+    {
+        Waypoint_Position = transform.position;
+        Waypoint_Rotation = transform.rotation.eulerAngles;
+
+        DeactivateVisuals();
+
+    }
+
+    private void DeactivateVisuals()
+    {
+        this.GetComponent<MeshRenderer>().enabled = false;
+        this.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
+    }
+
+}
